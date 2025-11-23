391644af6a11c89f2a11fecfa8b571b9dbccac1d
orbit07 <anzunden.1@gmail.com>
2025-11-23 17:52:17 +0900

    親テンプレート置き換え

--- astra/functions.php
+++ astra/functions.php
@@ -205,34 +205,3 @@ require_once ASTRA_THEME_DIR . 'inc/core/markup/class-astra-markup.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-filters.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-hooks.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-functions.php';
-
-/**
- * Register custom post types.
- */
-function astra_register_custom_post_types() {
-        $post_types = array(
-                'service'  => 'サービス',
-                'case'     => '事例',
-                'document' => '資料',
-                'movie'    => '動画',
-                'event'    => 'イベント',
-        );
-
-        foreach ( $post_types as $post_type => $label ) {
-                register_post_type(
-                        $post_type,
-                        array(
-                                'labels'       => array(
-                                        'name'          => $label,
-                                        'singular_name' => $label,
-                                ),
-                                'public'       => true,
-                                'has_archive'  => true,
-                                'show_in_rest' => true,
-                                'rewrite'      => array( 'slug' => $post_type ),
-                                'supports'     => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
-                        )
-                );
-        }
-}
-add_action( 'init', 'astra_register_custom_post_types' );

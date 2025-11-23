0d7d7179743f4ea6b489ed5c9f8fd4ac5773c21e
orbit07 <anzunden.1@gmail.com>
2025-11-23 18:08:04 +0900

    構成の調整

--- cmt/template-parts/layout/layout-1column.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <?php get_template_part( 'template-parts/common/breadcrumb' ); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php get_template_part( 'template-parts/article/content' ); ?>
-        <footer class="article-footer">
-            <?php get_template_part( 'template-parts/article/cta' ); ?>
-        </footer>
-    </article>
-</div>
